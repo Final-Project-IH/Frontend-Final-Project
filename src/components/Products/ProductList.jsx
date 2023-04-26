d48f7c17619cd120a942c7379f0f2edef9cb7b78
Daniel Olea <oleaonize@gmail.com>
2023-04-26 19:20:37 +0200

    Arreglada vista portatil con 100% zoom

--- src/components/Products/ProductList.jsx
+++ src/components/Products/ProductList.jsx
@@ -8,7 +8,7 @@ const ProductList = ({ auctions, statusFilter }) => {
       {auctions?.map((auction) => {
         if (statusFilter && auction.status === 'Closed') return null
         return (
-          <div className="d-flex justify-content-center">
+          <div className="d-flex">
         <div key={auction._id} className="col-xl-3 col-lg-3 col-md-6 col-sm-12 col-xs-12 listcards">
           <ProductCard product={auction?.product} auction={auction} />
         </div>
